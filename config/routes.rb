54e49c8e99c07f2991543e52ea62ed6a69715d6a
Nuttanart Pornprasitsakul <visibletrap@gmail.com>
2011-10-14 22:27:19 +0800

    Demonstrate view

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Fblikes::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'scores#main'
 
   # See how all your routes lay out with "rake routes"
 
