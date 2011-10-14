3c5af16d10c4f64851d25c41e2aeea9497d8ef9b
Nuttanart Pornprasitsakul <visibletrap@gmail.com>
2011-10-14 22:02:22 +0800

    Init fb likes count project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fblikes::Application.config.secret_token = 'cfe7100b217dc214ab019669b80c87f986011a5f15bca4b76c9ec5424e4d4e92474bb9dbc189f64b0aac631c4ba830c7ae2406ce401809a7df8a17e14825bd8f'
